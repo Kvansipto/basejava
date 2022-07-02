01446ea98d30de2d35dfd5cc8bfbcd20aa12858f
Антон Жданок <sheldor@MacBook-Air-Anton.local>
2022-07-02 22:30:02 +0700

    completed HW1 for Basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,51 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (Resume resume : getAll()) {
+            if (uuid.equals(resume.uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        Resume resume = get(uuid);
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].equals(resume)) {
+                storage[i] = storage[i + 1];
+                resume = storage[i + 1];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] newResumeArray = new Resume[size()];
+        if (size() >= 0) System.arraycopy(storage, 0, newResumeArray, 0, size());
+        return newResumeArray;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+            count++;
+        }
+        return count;
     }
 }
